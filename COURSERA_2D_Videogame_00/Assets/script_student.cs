27b360d91a115e91530c0c961c831b3cbcaad4d8
Heriberto Forero Correa <hforeroc@unal.edu.co>
2024-01-20 18:05:21 -0500

    correccion student

--- COURSERA_2D_Videogame_00/Assets/script_student.cs
+++ COURSERA_2D_Videogame_00/Assets/script_student.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class control_student : MonoBehaviour 
 {
-    public float vel = 1f;
-    public int symbol = -1;
+    
     private BoxCollider bxCol_right_wall;
     private BoxCollider bxCol_left_wall;
 
     float _horizontal = 0;
-    public float speed = 1f;
+    public float Direction = -1f;
+    public int Speed = 1;
     public Rigidbody _RigidBody;
     public Animator _Animator;
 
@@ -19,13 +19,14 @@ public class control_student : MonoBehaviour
     void Start()
     {
         _RigidBody = GetComponent<Rigidbody> ();
+        _Animator = GetComponent<Animator>();
 
         // Obtener la referencia al BoxCollider del objeto "invisibleleftwall"
         GameObject student_idle_left_wall = GameObject.Find("student_idle_left_wall");
         GameObject student_idle_right_wall = GameObject.Find("student_idle_right_wall");
 
         // Verificar si se encontró el objeto "invisibleleftwall"
-        if (student_idle_left_wall != null | student_idle_left_wall != null)
+        if (student_idle_left_wall != null || student_idle_left_wall != null)
         {
             // Obtener la referencia al BoxCollider
             bxCol_left_wall = student_idle_left_wall.GetComponent<BoxCollider>();
@@ -35,7 +36,7 @@ public class control_student : MonoBehaviour
             bxCol_left_wall.enabled = true;
 
             // Verificar si se encontró el BoxCollider
-            if (bxCol_left_wall == null | bxCol_right_wall == null)
+            if (bxCol_left_wall == null || bxCol_right_wall == null)
             {
                 Debug.LogError("No se encontraron los BoxCollider para Idle de Student.");
             }
@@ -49,7 +50,7 @@ public class control_student : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 vel_x = new Vector2 (vel, _RigidBody.velocity.y);
+        Vector2 vel_x = new Vector2(Direction * Speed, _RigidBody.velocity.y);
         _RigidBody.velocity = vel_x;
         _Animator.SetFloat("Anim_Speed", Mathf.Abs(vel_x.x));
     }
@@ -57,24 +58,24 @@ public class control_student : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     { 
         Flip();
-        symbol = symbol * -1;
+        Direction = Direction * -1;
     }
     void Flip()
     {
 
-        if (symbol == -1) 
+        if (Direction == -1) 
         {
             bxCol_left_wall.enabled = false;
             bxCol_right_wall.enabled = true;
         }
-        else if (symbol == 1) 
+        else if (Direction == 1) 
         {
             bxCol_right_wall.enabled = false;
             bxCol_left_wall.enabled = true;
         }
 
         Vector3 localScale = transform.localScale;
-        localScale.x *= symbol;
-        vel *= symbol;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
     }
 }
